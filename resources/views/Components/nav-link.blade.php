46af36fd3da656f4d117772599fbf28c43cef539
yiyikim <yiyikim11@gmail.com>
2025-05-22 21:30:52 +0700

    style the current active nav link-working

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,4 +1,4 @@
-@props (['active'])
+@props (['active' => false, 'type' =>  'a'])
 
-<a class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium" 
-    aria-current="{{ request()->is('/')? 'page' : 'false'}} ">{{$slot}}</a>
+<a class="{{ $active ? ' text-white bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium" 
+    {{$attributes}} >{{$slot}}</a>
