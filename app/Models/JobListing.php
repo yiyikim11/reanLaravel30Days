f480b2c698daf823ca1fa424bc1d0b7d1aafee2c
yiyikim <yiyikim11@gmail.com>
2025-06-03 23:21:33 +0700

    Pivot tables and BelongToManyRelationships

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -15,4 +15,9 @@ class JobListing extends Model
     {
         return $this->belongsTo(Employer::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class,, foreignPivotKey: "job_listing_id");
+    }
 }
